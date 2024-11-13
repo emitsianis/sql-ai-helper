b4098b846abc0af6dfb31191deccd336b8f5bde0
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-13 18:59:24 +0200

    Add: Improve the ais capabilities

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 
+from tools.report import write_report_tool
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
@@ -15,7 +16,12 @@ print(tables)
 
 prompt = ChatPromptTemplate(
     messages=[
-        SystemMessage(content=f"You are an AI that has access to an SQLite db.\n{tables}"),
+        SystemMessage(content=(
+            "You are an AI that has access to an SQLite db.\n"
+            f"The tables in the database are: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead use the 'describe_tables' function."
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
@@ -24,6 +30,7 @@ prompt = ChatPromptTemplate(
 tools = [
     run_query_tool,
     describe_tables_tool,
+    write_report_tool,
 ]
 
 agent = OpenAIFunctionsAgent(
@@ -38,4 +45,5 @@ agent_executor = AgentExecutor(
     tools=tools,
 )
 
-agent_executor("How many users have provided a shipping address?")
+agent_executor("Summarize the 5 most popular products and write the results in a report file.")
+# agent_executor("How many users have provided a shipping address?")
--- tools/sql.py
+++ tools/sql.py
@@ -1,6 +1,8 @@
 import sqlite3
+from typing import List
 
 from langchain_core.tools import Tool
+from pydantic.v1 import BaseModel
 
 conn = sqlite3.connect('db.sqlite')
 
@@ -22,10 +24,15 @@ def run_sqlite_query(query):
         return f"The following error occurred: {str(error)}"
 
 
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
+
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a query on the SQLite database",
     func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema,
 )
 
 
@@ -34,6 +41,10 @@ def describe_tables(table_names):
     tables = ', '.join("'" + table + "'" for table in table_names)
     rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});")
     return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
+class DescribeTablesArgsSchema(BaseModel):
+    table_names: List[str]
 
 
 describe_tables_tool = Tool.from_function(
