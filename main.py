df8c2aec5b6f2359b8ffcf01685a9f9273004889
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-13 17:24:23 +0200

    Add: Tool to describe tables

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from dotenv import load_dotenv
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+print(tables)
+
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=f"You are an AI that has access to an SQLite db.\n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
 tools = [
-    run_query_tool
+    run_query_tool,
+    describe_tables_tool,
 ]
 
 agent = OpenAIFunctionsAgent(
